69120b0b79e0019a178d356a954af8bc570b3c8f
jbaptistetrb <jbaptistetrb@users.noreply.github.com>
2020-03-02 22:56:05 +0100

    Solve troubles with icons on linux and windows

--- src/app.js
+++ src/app.js
@@ -1,52 +1,143 @@
 const {
   app,
+  ipcMain,
+  session,
   BrowserWindow,
-  Menu
+  Menu,
 } = require("electron")
+const fs = require("fs")
+const path = require("path")
 
-let mainWindow = null
 
-app.on("window-all-closed", function () {
-  if (process.platform != "darwin")
-    app.quit()
-  app.quit()
-})
+function addMenu(platform) {
+  let menu = Menu.buildFromTemplate([{
+      label: "Home",
+      submenu: [{
+          role: "about"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Services",
+          submenu: []
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "hide"
+        },
+        {
+          role: "hideOthers"
+        },
+        {
+          role: "unhide"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "quit"
+        },
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [{
+          role: "undo"
+        },
+        {
+          role: "redo"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "cut"
+        },
+        {
+          role: "copy"
+        },
+        {
+          role: "paste"
+        }
+      ]
+    }, {
+      label: "View",
+      submenu: [{
+          role: "reload"
+        },
+        {
+          role: "toggledevtools"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "resetzoom"
+        },
+        {
+          role: "zoomin"
+        },
+        {
+          role: "zoomout"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "togglefullscreen"
+        }
+      ]
+    }, {
+      role: "window",
+      submenu: [{
+          role: "minimize"
+        },
+        {
+          role: "close"
+        }
+      ]
+    }, {
+      role: "help",
+      submenu: [{
+        label: "Learn More"
+      }]
+    }
+  ])
 
-let template = [{
-    label: "Home",
-    submenu: [{
-        role: "about"
-      },
-      {
-        type: "separator"
-      },
-      {
-        label: "Services",
-        submenu: []
-      },
-      {
-        type: "separator"
-      },
-      {
-        role: "hide"
-      },
-      {
-        role: "hideOthers"
-      },
-      {
-        role: "unhide"
-      },
-      {
-        type: "separator"
-      },
-      {
-        role: "quit"
-      },
-    ]
-  },
-  {
-    label: "Edit",
-    submenu: [{
+  if (platform == "darwin") {
+    Menu.setApplicationMenu(menu)
+  } else {
+    Menu.setApplicationMenu(null)
+  }
+}
+
+
+function createWindow() {
+
+  // Create the browser window
+  mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    titleBarStyle: "hiddenInset",
+    icon: path.join(__dirname, "/build/icons/icon.png"),
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, "preload.js")
+    }
+  })
+
+  // Open the devtools
+  // mainWindow.openDevTools()
+
+  // Load the app
+  mainWindow.loadURL(`file://${__dirname}/pev/index.html`)
+
+  // On ipcEvent...
+  ipcMain.on("contextmenu:open", function (event, x, y) {
+    let contextmenu = Menu.buildFromTemplate([{
         role: "undo"
       },
       {
@@ -63,76 +154,61 @@ let template = [{
       },
       {
         role: "paste"
-      }
-    ]
-  }, {
-    label: "View",
-    submenu: [{
-        role: "reload"
-      },
-      {
-        role: "toggledevtools"
       },
       {
         type: "separator"
       },
       {
-        role: "resetzoom"
-      },
-      {
-        role: "zoomin"
-      },
-      {
-        role: "zoomout"
-      },
-      {
-        type: "separator"
-      },
-      {
-        role: "togglefullscreen"
+        label: "Advanced",
+        submenu: [{
+            role: "reload"
+          },
+          {
+            role: "toggledevtools"
+          },
+        ]
       }
-    ]
-  }, {
-    role: "window",
-    submenu: [{
-        role: "minimize"
-      },
-      {
-        role: "close"
-      }
-    ]
-  }, {
-    role: "help",
-    submenu: [{
-      label: "Learn More"
-    }]
-  }
-]
-
-menu = Menu.buildFromTemplate(template)
-
-function createWindow() {
-
-  mainWindow = new BrowserWindow({
-    width: 1600,
-    height: 900,
-    titleBarStyle: "hiddenInset",
-    webPreferences: {
-      nodeIntegration: false
-    }
+    ])
+    contextmenu.popup({
+      window: mainWindow,
+      x,
+      y
+    })
   })
 
-  mainWindow.loadURL(`file://${__dirname}/pev/index.html`)
+  if (process.platform == "darwin") {
+    app.on("activate-with-no-open-windows", function () {
+      mainWindow.show()
+    })
+  }
 
-  Menu.setApplicationMenu(menu)
-
+  // Emitted when the window is closed.
   mainWindow.on("close", function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
     if (process.platform != "darwin") {
       mainWindow = null
     } else {
-      mainWindow = null
+      mainWindow.hide()
     }
   })
 }
 
+
+// Keep a global reference of the window object, if you don"t, the window will
+// be closed automatically when the javascript object is GCed.
+let mainWindow = null
+
+// Add menu
+addMenu(process.platform)
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  if (process.platform != "darwin")
+    app.quit()
+})
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
 app.on("ready", createWindow)
